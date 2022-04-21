69ae2b802b45212f028a26ff345b96f312facde9
munkasirb <54366925+munkasirb@users.noreply.github.com>
2022-04-22 01:56:24 +1000

    Add files via upload
    My first Madlibs game

---
+++ Madlib1.py
@@ -0,0 +1,11 @@
+Flower_1= input("Name a flower: ")
+Color_1= input("Name a color: ")
+Flower_2= input("Name another flower: ")
+Color_2= input("Name another color: ")
+Name= input("input a name: ")
+
+
+
+print (Flower_1 +" are "+ Color_1)
+print (Flower_2 +" are "+ Color_2)
+print (Name+" loves you")
